463d8150fd44ee183be0a88e5241ada4301ab8b1
Max Huecksteadt <mhueck2@pdx.edu>
2022-10-19 17:49:10 -0700

    inital changes to all files

---
+++ hw2/model/Model.py
@@ -0,0 +1,21 @@
+class Model():
+    def select(self):
+        """
+        Gets all entries from the database
+        :return: Tuple containing all rows of database
+        """
+        pass
+
+    def insert(name, services, location, hours, phone, review):
+        """
+        Inserts entry into database
+        :param name: String
+        :param services: String
+        :param location: String
+        :param hours: String
+        :param phone: String
+        :param review: String 
+        :return: none
+        :raises: Database errors on connection and insertion
+        """
+        pass
